54fd98d68adfddd3352cdcbf41bc08ba44eb0a49
Dorota638 <1081600@ucn.dk>
2023-02-07 13:44:39 +0100

    repairs list

--- src/customers/Customers.tsx
+++ src/customers/Customers.tsx
@@ -15,8 +15,6 @@ import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
@@ -40,19 +38,19 @@ function createData(
 }
 
 const rows = [
-  createData('Cupcake', ' +4552741730', 'example@domain.com'),
-  createData('Donut', '+4552741730', 'example@domain.com'),
-  createData('Eclair', '+4552741730', 'example@domain.com'),
-  createData('Frozen yoghurt', '+4552741730', 'example@domain.com'),
-  createData('Gingerbread', '+4552741730', 'example@domain.com'),
-  createData('Honeycomb', '+4552741730', 'example@domain.com'),
-  createData('Ice cream sandwich', '+4552741730', 'example@domain.com'),
-  createData('Jelly Bean', '+4552741730', 'example@domain.com'),
-  createData('KitKat', '+4552741730', 'example@domain.com'),
-  createData('Lollipop', '+4552741730', 'example@domain.com'),
-  createData('Marshmallow', '+4552741730', 'example@domain.com'),
-  createData('Nougat', '+4552741730', 'example@domain.com'),
-  createData('Oreo', '+4552741730', 'example@domain.com'),
+  createData('Angil Maric', '+81 759 404 5717', 'amaric0@constantcontact.com'),
+  createData('Cecilia Kauble', '+7 165 292 6722', 'ckauble1@a8.net'),
+  createData('Angelle Rowes', '+385 329 511 2072', 'arowes2@va.gov'),
+  createData('Archambault Muggach', '+63 993 279 2605', 'amuggach3@de.vu'),
+  createData('Diann Kleinhandler', '+63 780 455 0949', 'dkleinhandler4@infoseek.co.jp'),
+  createData('Chelsie Philbrook', '+353 674 999 1993', 'cphilbrook5@nifty.com'),
+  createData('Brenda Stellino', '+7 539 331 4262', 'bstellino6@statcounter.com'),
+  createData('Maitilde Brunet', '+7 371 986 2117', 'mbrunet7@acquirethisname.com'),
+  createData('Arly Crain', '+386 739 972 0105', 'acrain8@cargocollective.com'),
+  createData('Brien Gymblett', '+351 107 907 6805', 'bgymblett9@e-recht24.de'),
+  createData('Ives Holhouse', '+66 997 401 8458', 'iholhousea@ask.com'),
+  createData('Jolee Paladino', '+57 525 994 7831', 'jpaladinob@chronoengine.com'),
+  createData('Karly Niccolls', '+86 125 210 1138', 'kniccollsc@google.it'),
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -236,7 +234,7 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
 
 export const Customers = () => {
   const [order, setOrder] = React.useState<Order>('asc');
-  const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
+  const [orderBy, setOrderBy] = React.useState<keyof Data>('name');
   const [selected, setSelected] = React.useState<readonly string[]>([]);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -458,242 +456,3 @@ export const Customers = () => {
 //       </React.Fragment>
 //     );
 // }
-
-
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
-// import AddIcon from '@mui/icons-material/Add';
-// import EditIcon from '@mui/icons-material/Edit';
-// import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-// import SaveIcon from '@mui/icons-material/Save';
-// import CancelIcon from '@mui/icons-material/Close';
-// import {
-//   GridRowsProp,
-//   GridRowModesModel,
-//   GridRowModes,
-//   DataGridPro,
-//   GridColumns,
-//   GridRowParams,
-//   MuiEvent,
-//   GridToolbarContainer,
-//   GridActionsCellItem,
-//   GridEventListener,
-//   GridRowId,
-//   GridRowModel,
-// } from '@mui/x-data-grid-pro';
-// import {
-//   randomCreatedDate,
-//   randomTraderName,
-//   randomUpdatedDate,
-//   randomId,
-// } from '@mui/x-data-grid-generator';
-
-// const initialRows: GridRowsProp = [
-//   {
-//     id: randomId(),
-//     name: randomTraderName(),
-//     age: 25,
-//     dateCreated: randomCreatedDate(),
-//     lastLogin: randomUpdatedDate(),
-//   },
-//   {
-//     id: randomId(),
-//     name: randomTraderName(),
-//     age: 36,
-//     dateCreated: randomCreatedDate(),
-//     lastLogin: randomUpdatedDate(),
-//   },
-//   {
-//     id: randomId(),
-//     name: randomTraderName(),
-//     age: 19,
-//     dateCreated: randomCreatedDate(),
-//     lastLogin: randomUpdatedDate(),
-//   },
-//   {
-//     id: randomId(),
-//     name: randomTraderName(),
-//     age: 28,
-//     dateCreated: randomCreatedDate(),
-//     lastLogin: randomUpdatedDate(),
-//   },
-//   {
-//     id: randomId(),
-//     name: randomTraderName(),
-//     age: 23,
-//     dateCreated: randomCreatedDate(),
-//     lastLogin: randomUpdatedDate(),
-//   },
-// ];
-
-// interface EditToolbarProps {
-//   setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
-//   setRowModesModel: (
-//     newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
-//   ) => void;
-// }
-
-// function EditToolbar(props: EditToolbarProps) {
-//   const { setRows, setRowModesModel } = props;
-
-//   const handleClick = () => {
-//     const id = randomId();
-//     setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
-//     setRowModesModel((oldModel) => ({
-//       ...oldModel,
-//       [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
-//     }));
-//   };
-
-//   return (
-//     <GridToolbarContainer>
-//       <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-//         Add record
-//       </Button>
-//     </GridToolbarContainer>
-//   );
-// }
-
-// export const Customers= () => {
-//   const [rows, setRows] = React.useState(initialRows);
-//   const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
-
-//   const handleRowEditStart = (
-//     params: GridRowParams,
-//     event: MuiEvent<React.SyntheticEvent>,
-//   ) => {
-//     event.defaultMuiPrevented = true;
-//   };
-
-//   const handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
-//     event.defaultMuiPrevented = true;
-//   };
-
-//   const handleEditClick = (id: GridRowId) => () => {
-//     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-//   };
-
-//   const handleSaveClick = (id: GridRowId) => () => {
-//     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-//   };
-
-//   const handleDeleteClick = (id: GridRowId) => () => {
-//     setRows(rows.filter((row) => row.id !== id));
-//   };
-
-//   const handleCancelClick = (id: GridRowId) => () => {
-//     setRowModesModel({
-//       ...rowModesModel,
-//       [id]: { mode: GridRowModes.View, ignoreModifications: true },
-//     });
-
-//     const editedRow = rows.find((row) => row.id === id);
-//     if (editedRow!.isNew) {
-//       setRows(rows.filter((row) => row.id !== id));
-//     }
-//   };
-
-//   const processRowUpdate = (newRow: GridRowModel) => {
-//     const updatedRow = { ...newRow, isNew: false };
-//     setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-//     return updatedRow;
-//   };
-
-//   const columns: GridColumns = [
-//     { field: 'name', headerName: 'Name', width: 180, editable: true },
-//     { field: 'age', headerName: 'Age', type: 'number', editable: true },
-//     {
-//       field: 'dateCreated',
-//       headerName: 'Date Created',
-//       type: 'date',
-//       width: 180,
-//       editable: true,
-//     },
-//     {
-//       field: 'lastLogin',
-//       headerName: 'Last Login',
-//       type: 'dateTime',
-//       width: 220,
-//       editable: true,
-//     },
-//     {
-//       field: 'actions',
-//       type: 'actions',
-//       headerName: 'Actions',
-//       width: 100,
-//       cellClassName: 'actions',
-//       getActions: ({ id }) => {
-//         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-//         if (isInEditMode) {
-//           return [
-//             <GridActionsCellItem
-//               icon={<SaveIcon />}
-//               label="Save"
-//               onClick={handleSaveClick(id)}
-//             />,
-//             <GridActionsCellItem
-//               icon={<CancelIcon />}
-//               label="Cancel"
-//               className="textPrimary"
-//               onClick={handleCancelClick(id)}
-//               color="inherit"
-//             />,
-//           ];
-//         }
-
-//         return [
-//           <GridActionsCellItem
-//             icon={<EditIcon />}
-//             label="Edit"
-//             className="textPrimary"
-//             onClick={handleEditClick(id)}
-//             color="inherit"
-//           />,
-//           <GridActionsCellItem
-//             icon={<DeleteIcon />}
-//             label="Delete"
-//             onClick={handleDeleteClick(id)}
-//             color="inherit"
-//           />,
-//         ];
-//       },
-//     },
-//   ];
-
-//   return (
-//     <Box
-//       sx={{
-//         height: 500,
-//         width: '100%',
-//         '& .actions': {
-//           color: 'text.secondary',
-//         },
-//         '& .textPrimary': {
-//           color: 'text.primary',
-//         },
-//       }}
-//     >
-//       <DataGridPro
-//         rows={rows}
-//         columns={columns}
-//         editMode="row"
-//         rowModesModel={rowModesModel}
-//         onRowModesModelChange={(newModel) => setRowModesModel(newModel)}
-//         onRowEditStart={handleRowEditStart}
-//         onRowEditStop={handleRowEditStop}
-//         processRowUpdate={processRowUpdate}
-//         components={{
-//           Toolbar: EditToolbar,
-//         }}
-//         componentsProps={{
-//           toolbar: { setRows, setRowModesModel },
-//         }}
-//         experimentalFeatures={{ newEditingApi: true }}
-//       />
-//     </Box>
-//   );
-// }
-
-
